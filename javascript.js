37331b36fe0008656fdddc25f2400ac894ec9893
Chandrasing610 <106746608+Chandrasing610@users.noreply.github.com>
2022-06-03 16:17:31 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,21 @@
+(function(){
+    $('form > input').keyup
+    (function(){
+   var empty=false;
+   $('form > input').each
+   (function(){
+    if ($(this).val()=='')
+    {
+        empty=true;
+    }
+   });
+   if(empty){
+        $('#register').attr('disabled','disabled');
+
+   }
+   else
+   {
+        $('#register').removeAttr('disable');
+   }
+ });
+})()
